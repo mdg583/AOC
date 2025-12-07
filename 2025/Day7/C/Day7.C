8cdee1b63e36eef5b22464b261756779cbe8b264
Matthew <14348557+mdg583@users.noreply.github.com>
2025-12-07 21:57:45 +0300

    Added C version

---
+++ 2025/Day7/C/Day7.C
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// cat input.txt | ./Day7.out
+
+int main() {
+    // First row: find S and count lines
+    int s_loc = -1;
+    int cols = 0;
+    char c = getchar();
+    while(c != EOF && c != '\n'){
+        if(c == 'S') s_loc = cols;
+        c = getchar(); cols++;
+    }
+    cols--;
+    // printf("S at %d, line is %d chars\n", s_loc, cols);
+
+    // initialize beams
+    long long *beams = (long long  *)malloc(sizeof(long long)*cols);
+    for(int i = 0; i < cols; i++) {
+        beams[i] = (i==s_loc);
+    }
+
+    // check flow
+    int i = 0;
+    while((c = getchar()) != EOF){
+        if(c == '\n') {
+            i = 0;
+            // for(int j = 0; j < cols; j++) printf("%2lld ", beams[j]); printf("\n");
+            continue;
+        }
+        if(c == '^') {
+            if(i > 0) beams[i-1] += beams[i];
+            if(i < cols-1) beams[i+1] += beams[i];
+            beams[i] = 0;
+        }
+        i++;
+    }
+    long long total = 0;
+    for(i = 0; i < cols; i++) total += beams[i];
+    printf("Beams: %llu\n",total);
+}
